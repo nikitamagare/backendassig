e48c794272648d976ba5becd9d28d2a5d228b93b
nikitamangare <nikitacmagare@gmail.com>
2020-05-15 16:53:56 +0530

    newtask

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 //now to insert or read rom the db we have to import that model file here first
 
 let movieDB = require("./models/movieDB.model.js")
@@ -25,11 +25,11 @@ mongoose.connect(url, {
 });
 
 app.post('/', async function(req,res){
-	let name = req.body.incomingName;
+	let reqName = req.body.incomingName;
 	let img = req.body.img;
 	let summary = req.body.summary;
 
-	let movie = new movieDB({name:name,img:img,summary:summary})
+	let movie = new movieDB({name:reqName,img:img,summary:summary})
 	await movie.save()
 
 	res.send('done saved successfully')
@@ -42,6 +42,24 @@ app.get('/fetchAll',async function(req,res){
 
 	let allDocs = await movieDB.find();
 	res.json(allDocs)
+});
+
+
+//get particular data given task by ceo
+
+
+
+
+app.get(`/movies/:movieToSearch`,async (request, response) =>  {
+  const movieToSearch = request.params.movieToSearch;
+  console.log(movieToSearch)
+  const getMovieWithName = await movieDB.find( { $text: { $search: movieToSearch } } )
+  console.log('this is OP',getMovieWithName);
+ 
+  if(getMovieWithName.length == 0){
+  	return response.send('No Movie Found With The String Provided')
+  }  
+  response.json(getMovieWithName)
 });
 
 app.listen(3000);
--- models/movieDB.model.js
+++ models/movieDB.model.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 //we declare our db structure here from that movie text file
 
 const movieDB = new mongoose.Schema({
-    name: String, 
-    img: String, 
+    name: {type:String,text:true}, 
+    img: {type:String,text:true}, 
     summary: String
 }, {
     timestamps: true
